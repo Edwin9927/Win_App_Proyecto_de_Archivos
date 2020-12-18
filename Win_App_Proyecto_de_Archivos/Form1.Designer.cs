e8a289bdd9a5e6e8848283a458ad275eaa803c28
Edwin9927 <edwinmanzanoquinzo@gmail.com>
2020-12-17 22:46:05 -0500

    Diseño de la interfaz principal

--- Win_App_Proyecto_de_Archivos/Form1.Designer.cs
+++ Win_App_Proyecto_de_Archivos/Form1.Designer.cs
@@ -29,13 +29,141 @@ namespace Win_App_Proyecto_de_Archivos
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataCine1 = new Win_App_Proyecto_de_Archivos.DataCine();
+            this.BarraTitulo = new System.Windows.Forms.Panel();
+            this.BtnVolumen = new System.Windows.Forms.Button();
+            this.MenuVertical = new System.Windows.Forms.Panel();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnSalir = new System.Windows.Forms.PictureBox();
+            this.BtnMin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataCine1)).BeginInit();
+            this.BarraTitulo.SuspendLayout();
+            this.MenuVertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnSalir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataCine1
+            // 
+            this.dataCine1.DataSetName = "DataCine";
+            this.dataCine1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // BarraTitulo
+            // 
+            this.BarraTitulo.BackColor = System.Drawing.Color.Black;
+            this.BarraTitulo.Controls.Add(this.BtnSalir);
+            this.BarraTitulo.Controls.Add(this.BtnMin);
+            this.BarraTitulo.Controls.Add(this.label1);
+            this.BarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.BarraTitulo.Name = "BarraTitulo";
+            this.BarraTitulo.Size = new System.Drawing.Size(1000, 38);
+            this.BarraTitulo.TabIndex = 0;
+            this.BarraTitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BarraTitulo_MouseDown);
+            // 
+            // BtnVolumen
+            // 
+            this.BtnVolumen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.BtnVolumen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnVolumen.FlatAppearance.BorderSize = 0;
+            this.BtnVolumen.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.BtnVolumen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnVolumen.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnVolumen.ForeColor = System.Drawing.Color.White;
+            this.BtnVolumen.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnVolumen.Location = new System.Drawing.Point(0, 44);
+            this.BtnVolumen.Name = "BtnVolumen";
+            this.BtnVolumen.Size = new System.Drawing.Size(200, 40);
+            this.BtnVolumen.TabIndex = 2;
+            this.BtnVolumen.Text = "Volumen";
+            this.BtnVolumen.UseVisualStyleBackColor = false;
+            // 
+            // MenuVertical
+            // 
+            this.MenuVertical.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.MenuVertical.Controls.Add(this.BtnVolumen);
+            this.MenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuVertical.Location = new System.Drawing.Point(0, 38);
+            this.MenuVertical.Name = "MenuVertical";
+            this.MenuVertical.Size = new System.Drawing.Size(200, 450);
+            this.MenuVertical.TabIndex = 1;
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(200, 38);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(800, 450);
+            this.panelContenedor.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(190)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(496, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CINES";
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.ErrorImage = null;
+            this.BtnSalir.Image = ((System.Drawing.Image)(resources.GetObject("BtnSalir.Image")));
+            this.BtnSalir.Location = new System.Drawing.Point(963, 4);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(30, 30);
+            this.BtnSalir.TabIndex = 9;
+            this.BtnSalir.TabStop = false;
+            // 
+            // BtnMin
+            // 
+            this.BtnMin.ErrorImage = null;
+            this.BtnMin.Location = new System.Drawing.Point(927, 4);
+            this.BtnMin.Name = "BtnMin";
+            this.BtnMin.Size = new System.Drawing.Size(30, 30);
+            this.BtnMin.TabIndex = 8;
+            this.BtnMin.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1000, 488);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.MenuVertical);
+            this.Controls.Add(this.BarraTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CINE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataCine1)).EndInit();
+            this.BarraTitulo.ResumeLayout(false);
+            this.BarraTitulo.PerformLayout();
+            this.MenuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.BtnSalir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnMin)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DataCine dataCine1;
+        private System.Windows.Forms.Panel BarraTitulo;
+        private System.Windows.Forms.Panel MenuVertical;
+        private System.Windows.Forms.Panel panelContenedor;
+        private System.Windows.Forms.Button BtnVolumen;
+        private System.Windows.Forms.PictureBox BtnSalir;
+        private System.Windows.Forms.PictureBox BtnMin;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Win_App_Proyecto_de_Archivos/Form1.cs
+++ Win_App_Proyecto_de_Archivos/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,42 @@ namespace Win_App_Proyecto_de_Archivos
         public Form1()
         {
             InitializeComponent();
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void BarraTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        #region Funcion para abrir paneles
+        private void AbrirFormEnPanel(object formhija)
+        {
+            if (this.panelContenedor.Controls.Count > 0)
+                this.panelContenedor.Controls.RemoveAt(0);
+            Form fh = formhija as Form;
+            fh.TopLevel = false;
+            fh.Dock = DockStyle.Fill;
+            this.panelContenedor.Controls.Add(fh);
+            this.panelContenedor.Tag = fh;
+            fh.Show();
+
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btninicio_Click(null, e);
+        }
+
+        private void btninicio_Click(object sender, EventArgs e)
+        {
+            AbrirFormEnPanel(new inicio());
         }
     }
 }
